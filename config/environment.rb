e1d5646fd706b4548407b64acf99a9a4f0452b9f
Darek Charkot <darcoxxx2@o2.pl>
2013-10-09 21:12:36 +0200

    first commit

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
-# Load the rails application
-require File.expand_path('../application', __FILE__)
+# # Load the rails application
+# require File.expand_path('../application', __FILE__)
 
-# Initialize the rails application
-Blog::Application.initialize!
+# # Initialize the rails application
+# Blog::Application.initialize!
